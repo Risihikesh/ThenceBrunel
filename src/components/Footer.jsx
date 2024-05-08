85cc4feea64d03c5abd725da5cccc1739d31db69
Rishikesh <krishikesh369@gmail.com>
2024-05-09 00:19:47 +0530

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "../CSS/Footer.css"
+
+export default function Footer() {
+    return (
+        <div className="footerdiv">
+            <p>©  Talup 2023. All rights reserved</p>
+            <div className="footerright">
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+            </div>
+        </div>
+    );
+}
+
