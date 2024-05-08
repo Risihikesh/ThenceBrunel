0fd9fa69b895ced1c63ca5eb3c78785809105217
Rishikesh <krishikesh369@gmail.com>
2024-05-09 00:35:07 +0530

    home description added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "../CSS/Home.css"
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -7,6 +7,10 @@ export default function Home() {
     return (
         <>
             <NavBar/>
+            <div className="homediv">
+                <h1 className='home1'>Success stories</h1>
+                <h1 className='home2'>Every success journey we&apos;ve encountered.</h1>
+            </div>
            <Footer/>
         </>
     );
