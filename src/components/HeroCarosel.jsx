15bc68b5c73dd1b77d577c144b609d642dcf88a5
Rishikesh <krishikesh369@gmail.com>
2024-05-09 01:24:50 +0530

    home hero completed

---
+++ src/components/HeroCarosel.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+import AppButton from "./AppButton";
+import "../CSS/HeroCarosel.css";
+
+function HeroCarosel() {
+  const slides = [
+    "Enhance fortune 50 company’s insights teams research capabilities",
+    "Maximize top-tier corporation's insights division's research efficacy",
+    "Boost multinational conglomerate's analysis unit's exploration potential",
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setCurrentSlide((prevSlide) =>
+            prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+        );
+    }, 3000); // Change slides every 3 seconds
+
+    return () => clearTimeout(timer);
+}, [currentSlide]);
+
+  return (
+    <div className="herocarouseldiv">
+      <p>{slides[currentSlide]}</p>
+      <div className="dots">
+        {slides.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === currentSlide ? "active" : "inactive"}`}
+            onClick={() => setCurrentSlide(index)}
+          ></span>
+        ))}
+      </div>
+      <AppButton onClick={() => setCurrentSlide((currentSlide + 1) % slides.length)} idname="black" bgcolor="black" color="white" style={{ display: 'inline-flex', alignItems: 'center' }}>
+      <span style={{ display: 'inline-flex', alignItems: 'center' }} >Explore More<span><svg id ="carouselsvg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.1716 6.9999L-5.92571e-07 6.9999L-7.67416e-07 8.9999L12.1716 8.9999L6.8076 14.3638L8.2218 15.778L16 7.9999L8.2218 0.221679L6.8076 1.63589L12.1716 6.9999Z" fill="white"/>
+</svg></span></span>
+
+      </AppButton>
+    </div>
+  );
+}
+
+export default HeroCarosel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import "../CSS/Home.css"
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Faq from '../components/Faq';
+import HomeImg from '../components/HomeImg';
+import HeroCarosel from '../components/HeroCarosel';
 
 export default function Home() {
     return (
@@ -11,6 +13,10 @@ export default function Home() {
             <div className="homediv">
                 <h1 className='home1'>Success stories</h1>
                 <h1 className='home2'>Every success journey we&apos;ve encountered.</h1>
+            </div>
+            <div className="caroseldiv">
+                <HomeImg/>
+                <HeroCarosel/>
             </div>
             <Faq/>
            <Footer/>
