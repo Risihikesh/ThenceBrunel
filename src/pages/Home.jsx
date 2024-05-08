1af9a21cac872c1d746928405fd17bf9f04f769d
Rishikesh <krishikesh369@gmail.com>
2024-05-09 00:59:49 +0530

    faq added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "../CSS/Home.css"
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Faq from '../components/Faq';
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
                 <h1 className='home1'>Success stories</h1>
                 <h1 className='home2'>Every success journey we&apos;ve encountered.</h1>
             </div>
+            <Faq/>
            <Footer/>
         </>
     );
